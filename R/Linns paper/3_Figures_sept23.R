8aa755f4209b6ee84d60be57eaa1417e54f9a152
linnvassvik <linnvassvik@gmail.com>
2023-09-12 16:16:15 +0200

    created figures

--- R/Linns paper/3_Figures_sept23.R
+++ R/Linns paper/3_Figures_sept23.R
@@ -2,6 +2,7 @@
 source("R/Linns paper/1_Import_Data.R")
 
 library(ggplot2)
+library(dplyr)
 
 AbundanceDoy <- pollination2 %>% 
   ggplot(aes( x = doy, y = flower.sum, color = stage)) +
@@ -9,4 +10,23 @@ AbundanceDoy <- pollination2 %>%
   geom_smooth() +
   facet_wrap(~ year.poll)
 ggsave(AbundanceDoy, filename = "Figures/AbundanceDoy.jpeg", height = 6, width = 8)
+
+
+
+# Create the boxplot
+SeedMass_Stage <- dat %>%
+  ggplot(aes(x = Stage, y = Seed_mass, fill = Stage)) +
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0, 0.03)) + #excluded two outliers in 2017
+  facet_grid(~ Year)
+ggsave(SeedMass_Stage, filename = "Figures/SeedMass_Stage.jpeg", height = 6, width = 8)  
+
+
+SeedNumber_Stage <- dat %>%
+  ggplot(aes(x = Stage, y = Seed_number, fill = Stage)) +
+  geom_boxplot()
+ggsave(SeedNumber_Stage, filename = "Figures/SeedNumber_Stage.jpeg", height = 6, width = 8)  
+
+
+
 
