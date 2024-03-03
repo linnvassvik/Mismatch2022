76bfb69655dbb72c895d74673ec668fd40cdc331
linnvassvik <linnvassvik@gmail.com>
2024-03-03 21:19:55 +0100

    small adjustments

--- R/Linns paper/3_Figures_new.R
+++ R/Linns paper/3_Figures_new.R
@@ -38,7 +38,9 @@ plot1 <- pollination2 %>%
   theme_minimal() +
   scale_fill_manual (values =  c("#CC9999", "#330000")) +
   scale_color_manual (values =  c("#CC9999", "#330000")) +
-  labs(x = "DOY", y = "Mean number of flowers", shape = "Year", color = "Year", fill = "Year") 
+  labs(x = "DOY", y = "Floral density", shape = "Year", color = "Year", fill = "Year") 
+
+
 
 #  ggplot() +
 # geom_jitter(data = Temperature_all, aes(x = doy, y = Temp_2016_ALR, color = "2016"), position = position_jitter(width = 1), size = 0.2) +
@@ -56,7 +58,19 @@ plot1 <- pollination2 %>%
    guides(color = guide_legend(override.aes = list(shape = c(16, 17)), title = "Year"),
           shape = guide_legend(override.aes = list(color = c("#CC9999", "#330000")), title = "Year")) +
    guides(color = "none") +
+   guides(shape = "none") +
    theme_minimal()
+ 
+plot4 <- Klima_Year %>% 
+  ggplot(aes(x = Year, y = Temperature, color = Year)) +
+  geom_boxplot() + 
+  geom_jitter(aes(shape = Year), alpha = 0.2)+
+  scale_color_manual(values = c("#CC9999", "#330000")) +
+  scale_shape_manual(values = c("2016" = 16, "2017" = 17)) +
+  labs(x = "Year", y = "", color = "") +
+  guides(color = "none") +
+  guides(shape = "none") +
+  theme_minimal()
 
 
 plot3 <- day_flower %>% 
@@ -66,10 +80,37 @@ plot3 <- day_flower %>%
   scale_fill_gradient(low = "#FFCC66", high = "#990000", na.value = NA) +
   scale_shape_manual(values = c(21, 1, 24, 2), guide = "none") +
   theme_minimal() +
-  labs(x = "Days since snowmelt", y = "Cumulative temperature above 0 °C", color = "Timing of snowmelt (DOY)", fill = "Timing of snowmelt (DOY)", shape = "Year") 
+  labs(x = "Days since snowmelt", y = "Growing degree days above 0 °C", color = "Timing of snowmelt (DOY)", fill = "Timing of snowmelt (DOY)", shape = "Year") 
 
 #fylte rundinger på 2 år, åpne 1 år
-PhenologyPlot <- ggarrange(plot2, legend = "bottom",
-                           ggarrange(plot1, plot3, ncol = 2, legend = "bottom", labels = c("b", "c")),
-                           nrow = 2, labels = "a")
+Pheno1 <- ggarrange(plot2, plot4, legend = "bottom", labels = c("a", "b"))
+Pheno2 <- ggarrange(plot1, plot3, legend = "bottom", labels = c("c", "d"))
+
+PhenologyPlot <- ggarrange(
+  ggarrange(plot2, plot4, legend = "bottom", labels = c("a", "b")),
+  ggarrange(plot1, plot3, legend = "bottom", labels = c("c", "d")),
+  ncol = 1,
+  nrow = 2
+)
+
 ggsave(PhenologyPlot, filename = "Figures/PhenologyPlot.jpeg", height = 10, width = 8)
+
+
+
+
+##########
+##########
+#########
+total_flowers_per_year <- pollination2 %>%
+  group_by(year.poll) %>%
+  summarise(total_flowers = sum(flower.sum))
+  
+ModelFl <- lm(flower.sum ~ year.poll, data = pollination2)
+summary(ModelFl)
+
+total_visits_per_year <- pollination2 %>%
+  group_by(year.poll) %>%
+  summarise(total_fly = sum(fly))
+
+ModelVis <- lm(fly ~ year.poll, data = pollination2)
+summary(ModelVis)
